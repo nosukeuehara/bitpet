d0af65c8c12344a82fbf6978213a35143f26ed5c
nosukeuehara <ryuunosuke06160616@gmail.com>
2026-08-11 09:19:39 +0900

    Remove target directory from version control

--- target/debug/deps/bitpet-ec38a30bbc58ebae.d
+++
@@ -1,5 +0,0 @@
-/Users/nosuke/Dev/bitpet/target/debug/deps/bitpet-ec38a30bbc58ebae.d: src/main.rs
-
-/Users/nosuke/Dev/bitpet/target/debug/deps/bitpet-ec38a30bbc58ebae: src/main.rs
-
-src/main.rs:
--- target/debug/deps/lifecycle-811b9afeeadb1592.d
+++
@@ -1,9 +0,0 @@
-/Users/nosuke/Dev/bitpet/target/debug/deps/lifecycle-811b9afeeadb1592.d: tests/lifecycle.rs Cargo.toml
-
-/Users/nosuke/Dev/bitpet/target/debug/deps/liblifecycle-811b9afeeadb1592.rmeta: tests/lifecycle.rs Cargo.toml
-
-tests/lifecycle.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
